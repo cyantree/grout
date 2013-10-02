285e3bfe8306dd4e151fad9d30e0b8b732cc5a80
cyantree <mail@cyantree.com>
2013-10-02 20:09:59 +0200

    + Events. Added option to prepend event registration.

--- Cyantree/Grout/Event/Events.php
+++ Cyantree/Grout/Event/Events.php
@@ -1,46 +1,57 @@
 <?php
 namespace Cyantree\Grout\Event;
 
-class Events{
-	private $_events = array();
-	public function join($event, $callback, $callbackData = null){
-		if(!isset($this->_events[$event])){
-			$this->_events[$event] = array();
-		}
+class Events
+{
+    private $_events = array();
 
-		$this->_events[$event][] = array($callback, $callbackData);
-	}
+    public function join($event, $callback, $callbackData = null, $prepend = false)
+    {
+        if (!isset($this->_events[$event])) {
+            $this->_events[$event] = array();
+        }
 
-	public function leave($event, $callback){
-		if(!isset($this->_events[$event])) return;
+        if ($prepend) {
+            array_unshift($this->_events[$event], array($callback, $callbackData));
+        } else {
+            $this->_events[$event][] = array($callback, $callbackData);
+        }
+    }
 
-		$callbacks = &$this->_events[$event];
+    public function leave($event, $callback)
+    {
+        if (!isset($this->_events[$event])) {
+            return;
+        }
 
-		$id = 0;
-		$count = count($callbacks);
-		while($id < $count){
-			if($callbacks[$id][0] == $callback){
-				array_splice($callbacks, $id, 1);
-				break;
-			}
-			$id++;
-		}
-	}
+        $callbacks = & $this->_events[$event];
 
-	public function trigger($type, $data = null){
-		$e = new Event();
-		$e->type = $type;
-		$e->data = $data;
+        $id = 0;
+        $count = count($callbacks);
+        while ($id < $count) {
+            if ($callbacks[$id][0] == $callback) {
+                array_splice($callbacks, $id, 1);
+                break;
+            }
+            $id++;
+        }
+    }
 
-		if(!isset($this->_events[$type])) return $e;
+    public function trigger($type, $data = null)
+    {
+        $e = new Event();
+        $e->type = $type;
+        $e->data = $data;
 
-		$callbacks = $this->_events[$type];
+        if (!isset($this->_events[$type])) return $e;
 
-		foreach($callbacks as $callback){
-			call_user_func($callback[0], $e, $callback[1]);
-			if($e->stopPropagation) break;
-		}
+        $callbacks = $this->_events[$type];
 
-		return $e;
-	}
+        foreach ($callbacks as $callback) {
+            call_user_func($callback[0], $e, $callback[1]);
+            if ($e->stopPropagation) break;
+        }
+
+        return $e;
+    }
 }
