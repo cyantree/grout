83dfacad59685c190fef7b35fe4c14d3e00ce5e0
Christoph Schreiber <schreiber@pluspol-interactive.de>
2014-10-23 12:02:57 +0200

    ImageContent: Added options minAspectRatio, maxAspectRatio. Reworked validity checks. ! ImageContent: Reworked error codes and messages.

--- Set/Contents/ImageContent.php
+++ Set/Contents/ImageContent.php
@@ -7,16 +7,20 @@ use Cyantree\Grout\Set\ContentRenderers\ImageContentRenderer;
 use Cyantree\Grout\Tools\FileTools;
 use Cyantree\Grout\Tools\ImageTools;
 use Cyantree\Grout\Types\FileUpload;
+use Cyantree\Grout\Types\ImageToolsCheckFileResult;
 
 class ImageContent extends Content
 {
     public $required = false;
 
-    public $maxFilesize = 0;
-    public $minWidth = 0;
-    public $minHeight = 0;
-    public $maxWidth = 0;
-    public $maxHeight = 0;
+    public $maxFilesize = null;
+    public $minWidth = null;
+    public $minHeight = null;
+    public $maxWidth = null;
+    public $maxHeight = null;
+    public $minAspectRatio = null;
+    public $maxAspectRatio = null;
+    public $allowRotated = false;
 
     public $saveDirectory;
     public $saveDirectoryUrl;
@@ -42,14 +46,20 @@ class ImageContent extends Content
 
         if ($errors === null) {
             $errors = new ArrayFilter(array(
-                    'notSelected' => _('Im Feld „%name%“ wurde kein Bild ausgewählt.'),
-                    'invalidImage' => _('Im Feld „%name%“ wurde kein gültiges Bild ausgewählt.'),
-                    'invalidFilesize' => _('Das Bild „%name%“ darf nicht größer als %size% MB sein.'),
-                    'tooSmall' => _('Das Bild „%name%“ muss mindestens %width%x%height% Pixel groß sein.'),
-                    'tooLarge' => _('Das Bild „%name%“ darf nicht größer als %width%x%height% Pixel sein.')
+                    'required' => _('Im Feld „%name%“ wurde kein Bild ausgewählt.'),
+                    'invalid' => _('Im Feld „%name%“ wurde kein gültiges Bild ausgewählt.'),
+                    'filesize' => _('Das Bild „%name%“ darf nicht größer als %size% MB sein.'),
+                    'aspectRatio' => _('Das Seitenverhältnis des Bildes „%name%“ muss zwischen %minRatio% und %maxRatio% liegen.'),
+                    'minAspectRatio' => _('Das Seitenverhältnis des Bildes „%name%“ muss wenigstens %ratio% betragen.'),
+                    'maxAspectRatio' => _('Das Seitenverhältnis des Bildes „%name%“ darf höchstens %ratio% betragen.'),
+                    'maxSize' => _('Das Bild „%name%“ darf nicht größer als %width%x%height% Pixel sein.'),
+                    'maxWidth' => _('Das Bild „%name%“ darf nicht breiter als %width% Pixel sein.'),
+                    'maxHeight' => _('Das Bild „%name%“ darf nicht höher als %height% Pixel sein.'),
+                    'minSize' => _('Das Bild „%name%“ muss mindestens %width%x%height% Pixel groß sein.'),
+                    'minWidth' => _('Die Breite des Bildes „%name%“ darf nicht weniger als %width% Pixel betragen.'),
+                    'minHeight' => _('Die Höhe des Bildes „%name%“ darf nicht weniger als %height% Pixel betragen.'),
             ));
         }
-
         return $errors->get($code);
     }
 
@@ -81,38 +91,71 @@ class ImageContent extends Content
     public function check()
     {
         if (!$this->data && !$this->uploadedFile && $this->required) {
-            $this->postError('notSelected');
+            $this->postError('required');
             return;
         }
 
         if ($this->uploadedFile) {
-            if ($this->maxFilesize && $this->uploadedFile->size > $this->maxFilesize) {
-                $filesize = round($this->maxFilesize / 1024 / 1024 * 10) / 10;
+            $image = ImageTools::checkFile(
+                $this->uploadedFile->file,
+                $this->uploadedFile->name,
+                $this->maxFilesize,
+                $this->minWidth,
+                $this->minHeight,
+                $this->maxWidth,
+                $this->maxHeight,
+                $this->minAspectRatio,
+                $this->maxAspectRatio,
+                $this->allowRotated
+            );
 
-                $this->postError('invalidFilesize', array('%size%' => $filesize));
-                return;
-            }
-            $image = ImageTools::checkFile($this->uploadedFile->file, $this->uploadedFile->name);
             if (!$image->success) {
-                $this->postError('invalidImage');
-                return;
-            }
+                $errors = new ArrayFilter(array_flip($image->errors));
+                
+                if ($errors->has(ImageToolsCheckFileResult::ERROR_FILESIZE)) {
+                    $filesize = round($this->maxFilesize / 1024 / 1024 * 10) / 10;
 
-            if ($this->minWidth || $this->maxWidth) {
-                $sizeX = imagesx($image->image);
-                $sizeY = imagesy($image->image);
+                    $this->postError('filesize', array('%size%' => $filesize));
+                }
+                
+                if ($errors->has(ImageToolsCheckFileResult::ERROR_INVALID)) {
+                    $this->postError('invalid');
+                }
+                
+                if ($this->minWidth && $this->minHeight && $errors->has(ImageToolsCheckFileResult::ERROR_MIN_SIZE)) {
+                    $this->postError('minSize', array('%width%' => $this->minWidth, '%height%' => $this->minHeight));
+                    
+                } elseif ($this->minWidth && $errors->has(ImageToolsCheckFileResult::ERROR_MIN_WIDTH)) {
+                    $this->postError('minWidth', array('%width%' => $this->minWidth));
+                    
+                } elseif ($this->minHeight && $errors->has(ImageToolsCheckFileResult::ERROR_MIN_HEIGHT)) {
+                    $this->postError('minHeight', array('%height%' => $this->minHeight));
+                }
 
-                if ($this->minWidth && ($sizeX < $this->minWidth || $sizeY < $this->minHeight)) {
+                if ($this->maxWidth && $this->maxHeight && $errors->has(ImageToolsCheckFileResult::ERROR_MIN_SIZE)) {
+                    $this->postError('maxSize', array('%width%' => $this->maxWidth, '%height%' => $this->maxHeight));
+
+                } elseif ($this->maxWidth && $errors->has(ImageToolsCheckFileResult::ERROR_MIN_WIDTH)) {
+                    $this->postError('maxWidth', array('%width%' => $this->maxWidth));
+
+                } elseif ($this->maxHeight && $errors->has(ImageToolsCheckFileResult::ERROR_MIN_HEIGHT)) {
+                    $this->postError('maxHeight', array('%height%' => $this->maxHeight));
+                }
+
+                if ($this->minAspectRatio && $this->maxAspectRatio && $errors->has(ImageToolsCheckFileResult::ERROR_ASPECT_RATIO)) {
                     $this->postError(
-                        'tooSmall',
-                        array('%width%' => $this->minWidth, '%height%' => $this->minHeight)
+                        'aspectRatio',
+                        array(
+                            '%minRatio%' => number_format($this->minAspectRatio, 1),
+                            '%maxRatio%' => number_format($this->maxAspectRatio, 1)
+                        )
                     );
 
-                } elseif ($this->maxWidth && ($sizeX > $this->maxWidth || $sizeY > $this->maxHeight)) {
-                    $this->postError(
-                        'tooLarge',
-                        array('%width%' => $this->maxWidth, '%height%' => $this->maxHeight)
-                    );
+                } elseif ($this->minAspectRatio && $errors->has(ImageToolsCheckFileResult::ERROR_MIN_ASPECT_RATIO)) {
+                    $this->postError('minAspectRatio', array('%min%' => number_format($this->minAspectRatio, 1)));
+
+                } elseif ($this->maxAspectRatio && $errors->has(ImageToolsCheckFileResult::ERROR_MAX_ASPECT_RATIO)) {
+                    $this->postError('maxAspectRatio', array('%max%' => number_format($this->maxAspectRatio, 1)));
                 }
             }
 
