057dad5efe3081fa0222770d40a6555fd38d102e
cyantree <mail@cyantree.com>
2015-01-28 23:42:13 +0100

    * SetListResult: Added property $set.

--- Set/SetListResult.php
+++ Set/SetListResult.php
@@ -3,7 +3,16 @@ namespace Cyantree\Grout\Set;
 
 abstract class SetListResult
 {
+    /** @var Set */
+    public $set;
+
     public $countAll;
+
+    function __construct(Set $set)
+    {
+        $this->set = $set;
+    }
+
 
     abstract public function getNext();
 }
